5f1a3ac334b749c8f15f22e2a517f8af83221c59
Vasmayer <lealimarco90@gmail.com>
2021-12-20 15:20:49 +0100

    problem analysis added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 /* 
+Traccia:
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+Bonus:
+scrivere "La tua password è nomecogonomecolorepreferito21"
+Abbellire la pagina dal punto di vista grafico a piacimento!
 
+*********************** PROCEDURA OPERATIVA ***********************
 
+1.chiedere all'utente il nome tramite un prompt()
+2.salvare il nome in una costante
+3.chiedere il cognome
+4.salvare il cognome in una costante
+5.chiedere colore preferito
+6.salvare in una costante il colore preferito
+7.salvo il result la concatenazione delle varie stringhe aggiungendo 21
+8.recupero l'elemento che racchiude il testo
+9.copio il valore risult all'interno del div con un'innerHTML
 
 
 
